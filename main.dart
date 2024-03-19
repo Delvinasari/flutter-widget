28cd81728ab1c12096bb3d25ebd3cc2c1d1c6e90
Delvinasari <163649432+Delvinasari@users.noreply.github.com>
2024-03-19 23:20:53 +0700

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(AplikasiSaya());
+
+class AplikasiSaya extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+   return MaterialApp(
+     home: Scaffold(
+      appBar: AppBar(title: Text("Aplikasi")), 
+      body: Text("Ini bagian isi"),),);
+  }
+
+}
